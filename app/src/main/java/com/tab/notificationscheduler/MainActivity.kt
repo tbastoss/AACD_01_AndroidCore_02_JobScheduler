89df64245eab125dc1ba0792e1bcd70c7f1fe900
Thiago Bastos <alves_thiagobastos@hotmail.com>
2020-09-01 11:38:55 -0300

    complete code lab

--- app/src/main/java/com/tab/notificationscheduler/MainActivity.kt
+++ app/src/main/java/com/tab/notificationscheduler/MainActivity.kt
@@ -1,11 +1,107 @@
 package com.tab.notificationscheduler
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.job.JobInfo
+import android.app.job.JobScheduler
+import android.content.ComponentName
+import android.content.Context
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
+import android.widget.Switch
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.tab.notificationscheduler.databinding.ActivityMainBinding
+
+const val JOB_ID: Int = 0
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    lateinit var btnSchedule: Button
+    lateinit var btnCancelJob: Button
+    lateinit var mDeviceIdleSwitch: Switch
+    lateinit var mDeviceChargingSwitch: Switch
+    lateinit var mSeekBar: SeekBar
+
+    lateinit var mScheduler: JobScheduler
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        btnSchedule = binding.btnScheduleJob
+        btnCancelJob = binding.btnCancelJob
+        mDeviceIdleSwitch = binding.idleSwitch
+        mDeviceChargingSwitch = binding.chargingSwitch
+        mSeekBar = binding.seekBar
+        val seekBarProgress = binding.seekBarProgress
+
+        btnSchedule.setOnClickListener { v: View? -> scheduleJob() }
+        btnCancelJob.setOnClickListener { v: View? -> cancelJobs() }
+
+        mSeekBar.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                if (progress > 0){
+                    seekBarProgress.setText("$progress s");
+                }else {
+                    seekBarProgress.setText(R.string.not_set);
+                }
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
+    }
+
+    fun scheduleJob() {
+        mScheduler = getSystemService(Context.JOB_SCHEDULER_SERVICE) as JobScheduler
+        val networkOptions = binding.networkOptions
+        val selectedNetworkID = networkOptions.checkedRadioButtonId
+        val selectedNetworkOption = when(selectedNetworkID) {
+            binding.anyNetwork.id -> JobInfo.NETWORK_TYPE_ANY
+            binding.wifiNetwork.id -> JobInfo.NETWORK_TYPE_UNMETERED
+            else -> JobInfo.NETWORK_TYPE_NONE
+        }
+        val seekBarInteger = mSeekBar.progress
+        val seekBarSet = seekBarInteger > 0
+
+        val serviceName = ComponentName(packageName, NotificationJobService::class.java.name)
+        val builder = JobInfo.Builder(JOB_ID, serviceName)
+            .setRequiredNetworkType(selectedNetworkOption)
+            .setRequiresDeviceIdle(mDeviceIdleSwitch.isChecked)
+            .setRequiresCharging(mDeviceChargingSwitch.isChecked)
+
+        if (seekBarSet) {
+            builder.setOverrideDeadline((seekBarInteger * 1000).toLong());
+        }
+
+        val constrantSet = (selectedNetworkOption != JobInfo.NETWORK_TYPE_NONE) ||
+                mDeviceIdleSwitch.isChecked || mDeviceChargingSwitch.isChecked || seekBarSet
+
+
+
+        //Jobs can only be scheduled with at least one constraint
+        if (constrantSet) {
+            //Schedule job
+            val jobInfo = builder.build()
+            mScheduler.schedule(jobInfo)
+
+            Toast.makeText(this, "Job Scheduled, job will run when " +
+                    "the constraints are met.", Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(this, "Please set at least one constraint",
+                Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    fun cancelJobs() {
+        mScheduler.cancelAll()
+        Toast.makeText(this, "Jobs cancelled", Toast.LENGTH_SHORT).show()
     }
 }
---
+++ app/src/main/java/com/tab/notificationscheduler/NotificationJobService.kt
@@ -0,0 +1,75 @@
+package com.tab.notificationscheduler
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.job.JobParameters
+import android.app.job.JobService
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.os.Build
+import androidx.core.app.NotificationCompat
+
+const val PRIMARY_CHANNEL_ID: String = "primary_notification_channel"
+const val NOTIFICATION_ID: Int = 0
+
+class NotificationJobService : JobService() {
+
+    lateinit var mNotificationManager: NotificationManager
+
+    override fun onStopJob(params: JobParameters?): Boolean {
+        return true
+    }
+
+    override fun onStartJob(params: JobParameters?): Boolean {
+        //Create Notification Channel
+        createNotificationChannel()
+
+        //Set content intent
+        val intent = Intent(this, MainActivity::class.java)
+        val contentPendingIntent = PendingIntent.getActivity(
+            this,
+            NOTIFICATION_ID,
+            intent,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+
+        val builder = createNotification(contentPendingIntent)
+        mNotificationManager.notify(NOTIFICATION_ID, builder.build())
+
+        return false
+    }
+
+    private fun createNotification(contentPendingIntent: PendingIntent): NotificationCompat.Builder {
+        return NotificationCompat.Builder(this, PRIMARY_CHANNEL_ID)
+            .setContentTitle(getString(R.string.notification_title))
+            .setContentText(getString(R.string.notification_content_text))
+            .setContentIntent(contentPendingIntent)
+            .setSmallIcon(R.drawable.ic_baseline_refresh_24)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setDefaults(NotificationCompat.DEFAULT_ALL)
+            .setAutoCancel(true)
+    }
+
+    private fun createNotificationChannel() {
+        //Get Notificaion Manager
+        mNotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        // Check SDK version since Notif. Channel is only available on Oreo or above
+        if (Build.VERSION.SDK_INT >=
+                Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(
+                PRIMARY_CHANNEL_ID,
+                getString(R.string.notification_channel_name),
+                NotificationManager.IMPORTANCE_HIGH
+            )
+            notificationChannel.enableLights(true)
+            notificationChannel.setLightColor(Color.RED)
+            notificationChannel.enableVibration(true)
+            notificationChannel.setDescription(getString(R.string.notification_channel_description))
+
+            mNotificationManager.createNotificationChannel(notificationChannel)
+        }
+    }
+}
